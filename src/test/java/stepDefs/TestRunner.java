41a8341c64c19534685aec4574468c70596168f8
chithraup123 <chithra.devikripa@gmail.com>
2022-04-10 09:52:24 +0530

    [master] Add runner file

--- src/test/java/stepDefs/TestRunner.java
+++ src/test/java/stepDefs/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-		plugin = {"html:target/cucumber-html"},
+		plugin = {"html:target/cucumber-html", "json:target/cucumber.json"},
 		features = "src/test/java/features",
 		// tags = { "@Sanity"}--> for single
 		
@@ -20,7 +20,7 @@ import io.cucumber.junit.CucumberOptions;
 		
 		// if we have 100 scenarios but if some is getting failed in jenkins job, for time timebeing if we want ot ignore those then use this
 		//tags = { "not @Rapid" },
-		 tags = { "@Sanity and not @Rapid" },
+		 tags = { "@Sanity and not @Rapid", "@Regression"},
 		
 		glue = { "stepDefs" })
 public class TestRunner {
